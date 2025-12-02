0bf05eb4654318a1179f8f190c18e4e8329214bb
2terrrr <justinebenedict.bautista.sci@ust.edu.ph>
2025-12-03 01:11:17 +0800

    file path

--- DirtyCafe_DataCleaning.py
+++ DirtyCafe_DataCleaning.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 from google.colab import drive
 drive.mount('/content/drive')
 
+file_path = '/content/drive/MyDrive/Colab Notebooks/Dirty Cafe Data Cleaning/dirty_cafe_sales.csv'
+df = pd.read_csv(file_path)
+#import the dirty cafe csv
