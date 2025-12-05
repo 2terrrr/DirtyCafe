4586105d36cb4d1cdfe031c8a90e6873288eb27e
2terrrr <justinebenedict.bautista.sci@ust.edu.ph>
2025-12-05 15:55:48 +0800

    .

--- DirtyCafe_DataCleaning.py
+++ DirtyCafe_DataCleaning.py
@@ -25,3 +25,14 @@ df["Transaction Date"] = pd.to_datetime(df['Transaction Date'], errors='coerce')
 
 dirty_labels = ["ERROR", "UNKNOWN", "Missing value", "missing value", "nan", "", " "]
 df = df.replace(dirty_labels, np.nan)
+
+standard_prices = {
+    "Cake": 3.0,
+    "Coffee": 2.0,
+    "Cookie": 1.0,
+    "Juice": 3.0,
+    "Salad": 5.0,
+    "Sandwich": 4.0,
+    "Smoothie": 4.0,
+    "Tea": 1.5
+}
