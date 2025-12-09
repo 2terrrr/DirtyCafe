910168a38075b213e1ef084266c56a7a60cfffc7
2terrrr <justinebenedict.bautista.sci@ust.edu.ph>
2025-12-10 03:04:52 +0800

    .

--- DirtyCafe_DataCleaning.py
+++ DirtyCafe_DataCleaning.py
@@ -68,6 +68,7 @@ df = df.dropna(subset=['Quantity', 'Price Per Unit']).copy()
 
 profile = ProfileReport(df, title="Dirty Cafe Sales Report", explorative=True)
 #to check if everything worked or no
+#go to alerts and check 1 by 1
 
 display(profile.to_notebook_iframe())
 
